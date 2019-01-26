d56fc020f82525c21506daf2cfbb787230014f1b
Haz Ryder <hazryder@gmail.com>
2019-01-26 18:56:47 +0000

    Fixed a bug where player 2 wouldnt flip correctly.

---
+++ UnityProject/Assets/Scripts/Events/EndCharacterSelect.cs
@@ -0,0 +1,14 @@
+﻿namespace Events
+{
+    public class EndCharacterSelect
+    {
+        public int PlayerOneSelectedCharacter;
+        public int PlayerTwoSelectedCharacter;
+
+        public EndCharacterSelect(int playerOneCharacterSelect, int playerTwoCharacterSelect)
+        {
+            this.PlayerOneSelectedCharacter = playerOneCharacterSelect;
+            this.PlayerTwoSelectedCharacter = playerTwoCharacterSelect;
+        }
+    }
+}
